137a3142949be80b2c076a4dd6e2877f1b07beec
Christopher Guess <cguess@gmail.com>
2023-10-04 12:50:45 -0400

    Bump version

--- lib/birdsong.rb
+++ lib/birdsong.rb
@@ -10,9 +10,8 @@ require "fileutils"
 require_relative "birdsong/version"
 require_relative "birdsong/tweet"
 require_relative "birdsong/user"
-require_relative "birdsong/scrapers/scraper"
+# require_relative "birdsong/scrapers/scraper"
 require_relative "birdsong/scrapers/tweet_scraper"
-require_relative "birdsong/twitter_guest_key"
 
 require_relative "birdsong/monkeypatch"
 
